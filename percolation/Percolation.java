ebc3875712c2cb6bef775b79a307b05f9b14689c
Jason Wu <jason.k.wu@gmail.com>
2014-10-16 20:38:39 -0400

    adding percolation model using union find data structure.  still need to fix backwash issue.

---
+++ percolation/Percolation.java
@@ -0,0 +1,173 @@
+public class Percolation {
+    public int N;
+    public boolean [][] sites;
+    private WeightedQuickUnionUF sitesUF;
+    
+    public Percolation(int N) {
+        // Throw exception if N is <= 0
+        if (N <= 0) throw new 
+            IllegalArgumentException("N cannot be less than or equal to 0.");
+        
+        this.N = N;
+        sites = new boolean[N][N];
+        
+        // Init all sites to blocked (boolean false)
+        for (int i = 0; i<N; i++) {
+            for (int j = 0; j<N; j++) {
+                sites[i][j] = false;
+            }
+        }
+     
+        // Initialize unionfind object.  Have extra two sites to symbolize
+        // sites connected to the top or bottom.
+        // N^2 is associated with an open top site.
+        // N^2+1 is associated with an open bottom site.
+        // Note: backwash is due to virtual bottom site connected to
+        // all bottom sites.  
+        sitesUF = new WeightedQuickUnionUF(N*N+2);
+    }
+    
+    // In hindsight, it may have been easier to have extra rows and columns
+    // on the perimeter of the grid.
+    public void open (int i , int j) {
+        // Throws exception if index is out of bounds.
+        // Out of bounds if > N or < 1
+        if (i < 1 | i > N+1) throw new 
+            IndexOutOfBoundsException("Index i out of bounds.");
+        if (j < 1 | j > N+1) throw new 
+            IndexOutOfBoundsException("Index j out of bounds.");
+        
+        // open if blocked
+        if (!this.isOpen(i, j)) {
+            sites[i-1][j-1] = true;
+        
+            // Check to see if surrounding sites are also open.
+            // If so, connect the elements using WeightedQuickUnionUF
+            // Check bounds
+            // Above
+            if (i > 1) {
+                if (this.isOpen(i-1, j)) {
+                    sitesUF.union(xyTo1D(i, j), xyTo1D(i-1,j));
+                }
+            }
+            // If top row, connectto N^2 element (root of top open sites).
+            else {
+                sitesUF.union(xyTo1D(i, j), gridSize());
+            }
+            
+            // Below
+            if (i < N) {
+                if (this.isOpen(i+1, j)) {
+                    sitesUF.union(xyTo1D(i, j), xyTo1D(i+1,j));
+                }
+            }
+            // If bottom row, connect to N^2+1 element 
+            // (root of bottom open sites).
+            // Only connect to virtual site if the current site being
+            // opened is already full.  This will prevent backwashing
+            // from occuring.
+            else {
+                 sitesUF.union(xyTo1D(i, j), gridSize()+1);
+            }
+            
+            // To the right.
+            if (j > 1) {
+                if (this.isOpen(i, j-1)) {
+                    sitesUF.union(xyTo1D(i, j), xyTo1D(i,j-1));
+                }
+            }
+            
+            // To the left.
+            if (j < N) {
+                if (this.isOpen(i, j+1)) {
+                    sitesUF.union(xyTo1D(i, j), xyTo1D(i,j+1));
+                }
+            }
+        }
+    }
+    
+    public boolean isOpen(int i, int j) {
+        // Throws exception if index is out of bounds.
+        // Out of bounds if > N or < 1
+        if (i < 1 | i > N+1) throw new 
+            IndexOutOfBoundsException("Index i out of bounds.");
+        if (j < 1 | j > N+1) throw new 
+            IndexOutOfBoundsException("Index j out of bounds.");
+        
+        // return true if open, false if blocked.
+        return sites[i-1][j-1];
+    }
+    
+    // isFull returns true when we're connected to the root node that
+    // symbolizes a top connection, i.e. N^2 element in the data structure.
+    public boolean isFull(int i, int j) {
+        // Throws exception if index is out of bounds.
+        // Out of bounds if > N or < 1
+        if (i < 1 | i > N+1) throw new 
+            IndexOutOfBoundsException("Index i out of bounds.");
+        if (j < 1 | j > N+1) throw new 
+            IndexOutOfBoundsException("Index j out of bounds.");
+        
+        if (sitesUF.connected(xyTo1D(i,j), gridSize())) return true;
+        
+        return false;
+    }
+    
+    public boolean percolates() {
+        // Percolates if N^2 element of union find data structure is connected
+        // to N^2+1 element.  (top connected to bottom root respectively).
+        if (sitesUF.connected(gridSize(), gridSize()+1)) return true;
+        
+        return false;
+    }
+
+    private int xyTo1D(int i, int j) {
+        int retval;
+        
+        retval = (i-1)*N + j-1;
+        
+        return retval;
+    }
+    
+    private int gridSize() {
+        return (this.N*this.N);
+    }
+    
+    // Quick testing
+    public static void main(String [] args) {
+        int N_in = Integer.parseInt(args[0]);
+        Percolation perc = new Percolation(N_in);
+        print_grid(perc);
+        perc.open(1,1);
+        StdOut.printf("Opening (1,1)...\n");
+        print_grid(perc);
+        perc.open(1,perc.N);
+        StdOut.printf("Opening (1,%d)...\n", perc.N);
+        print_grid(perc);
+        StdOut.printf("(1,1) connected to top: ");
+        StdOut.println(perc.sitesUF.connected(
+                                              perc.xyTo1D(1,1),
+                                              perc.gridSize()));
+        StdOut.printf("Opening entire first column...\n");
+        for (int i = 0; i<perc.N; i++) perc.open(i+1, 1);
+        print_grid(perc);
+        StdOut.printf("(%d,1) connected to bottom: ", perc.N);
+        StdOut.println(perc.sitesUF.connected(
+                                              perc.xyTo1D(perc.N,1),
+                                              perc.gridSize()+1));
+        StdOut.printf("Percolates: %b\n", perc.percolates());
+        StdOut.printf("Is (%d,1) full: %b\n", 
+                      (int)perc.N/2, perc.isFull((int)perc.N/2,1));
+        
+    }
+    
+    private static void print_grid(Percolation perc) {
+        for (int i = 0; i<perc.N; i++) {
+            for (int j = 0; j<perc.N; j++) {
+                StdOut.print(perc.isOpen(i+1,j+1));
+                StdOut.printf(" ");
+            }
+            StdOut.printf("\n");
+        }
+    }
+}
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,129 @@
+public class PercolationStats {
+    public int N;
+    public int T;
+    private Percolation perc;
+    private double [] x; // estimation of mean/fraction.
+    
+    public PercolationStats(int N, int T) {
+        if (N<=0 || T<= 0) throw new 
+            IllegalArgumentException("N or T is <= 0");
+        
+        this.N = N;
+        this.T = T;
+        
+        perc = new Percolation(N);
+        x = new double[T];
+    }
+    
+    public double mean() {
+        double m = StdStats.mean(x);
+        return m;
+    }
+    
+    public double stddev() {
+        double std = 1;
+        if (T == 1) return Double.NaN;
+        std = StdStats.stddev(x);
+        return std;
+    }
+    
+    public double confidenceLo() {
+        double confLo = 0;
+        confLo = mean() - ((1.96*stddev())/Math.sqrt(T));
+        return confLo;
+    }
+    
+    public double confidenceHi() {
+        double confHi = 0;
+        confHi = mean() + ((1.96*stddev())/Math.sqrt(T));
+        return confHi;
+    }
+    
+    public static void main(String[] args) {
+        int N_in = 0;
+        int T_in = 0;
+        String outputFile = "";
+        boolean outToFile = false;
+        int count = 0; // count number of open sites;
+        Out outFile = new Out();
+  
+        // Check args are there
+        if (args.length > 0) {
+            try {
+                N_in = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                StdOut.printf("First argument not a number.\n");
+                System.exit(1);
+            }
+            
+            try {
+                T_in = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                StdOut.printf("Second argument not a number.\n");
+                System.exit(1);
+            }
+        }
+        
+        // Get third argument if it exists for output file for visualizer.
+        if (args.length >= 3) {
+            outputFile = args[2];
+            outToFile = true;
+        }
+        
+        // Don't do anything if N <= 0 or T <= 0
+        if (N_in <= 0 || T_in <= 0) return;
+        
+        PercolationStats percStats = new PercolationStats(N_in, T_in);
+        
+        // Perform experiment T times and record the results.
+        for (int t = 0; t < percStats.T; t++) {
+            // reset count
+            count = 0;
+
+            if (outToFile) {
+                outFile = new Out(outputFile + Integer.toString(t) + ".txt");
+                outFile.println(percStats.N);
+            }
+            
+            // Loop until percolates
+            while (!percStats.perc.percolates()) {
+                // Open a site at random.
+                int randi = StdRandom.uniform(percStats.N)+1;
+                int randj = StdRandom.uniform(percStats.N)+1;
+            
+                // Increase counter if a new site is opened.
+                if (!percStats.perc.isOpen(randi,randj)) count++;
+                percStats.perc.open(randi, randj);
+                
+                // Record randi and randj to file for visualizer.
+                if (outToFile) {
+                    outFile.print(randi);
+                    outFile.printf(" ");
+                    outFile.println(randj);
+                }
+            
+            }
+
+            double fraction = (double) count/(double) percStats.gridSize();
+            percStats.x[t] = fraction;
+            
+            // We're done with this percolation data structure.  Create
+            // a new one.  Hope garbage collection does it's job.
+            percStats.perc = new Percolation(percStats.N);
+        } 
+        
+        // output
+        StdOut.printf("mean                    = ");
+        StdOut.println(percStats.mean());
+        StdOut.printf("stddev                  = ");
+        StdOut.println(percStats.stddev());
+        StdOut.printf("95%% confidence interval = ");
+        StdOut.print(percStats.confidenceLo());
+        StdOut.printf(", ");
+        StdOut.println(percStats.confidenceHi());
+    }
+    
+    private int gridSize() {
+        return (this.N*this.N);
+    }
+}
